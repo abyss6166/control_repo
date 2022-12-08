74daffd3e4e78d0a7570f8829574993635e6d524
abyss6166 <54426650+abyss6166@users.noreply.github.com>
2022-12-08 14:39:00 -0500

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -6,8 +6,14 @@ class minecraft {
     ensure => file,
     source => 'https://piston-data.mojang.com/v1/objects/c9df48efed58511cdd0213c56b9013a7b5c9ac1f/server.jar',
   }
-  package {'java':
+  file {'/tmp/jdk-19_linux-x64_bin.rpm':
+    ensure => file,
+    source => 'https://download.oracle.com/java/19/latest/jdk-19_linux-x64_bin.rpm'
+  }
+  package {'jdk':
+    provider => 'rpm',
     ensure => present,
+    source => '/tmp/jdk-19_linux-x64_bin.rpm'
   }
   file {'/opt/minecraft/eula.txt':
     ensure => file,
