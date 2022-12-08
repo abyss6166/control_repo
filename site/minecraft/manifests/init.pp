f07d418e6916e55813af0d46182d8cf21f6f28d7
abyss6166 <54426650+abyss6166@users.noreply.github.com>
2022-12-08 14:57:35 -0500

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,7 +1,8 @@
-class minecraft {
-$serverurl='https://piston-data.mojang.com/v1/objects/c9df48efed58511cdd0213c56b9013a7b5c9ac1f/server.jar'
-$jdkurl='https://download.oracle.com/java/19/latest/jdk-19_linux-x64_bin.rpm'
+class minecraft (
+$serverurl='https://piston-data.mojang.com/v1/objects/c9df48efed58511cdd0213c56b9013a7b5c9ac1f/server.jar',
+$jdkurl='https://download.oracle.com/java/19/latest/jdk-19_linux-x64_bin.rpm',
 $install_dir='/opt/minecraft'
+){
   file {$install_dir:
     ensure => directory,
   }
